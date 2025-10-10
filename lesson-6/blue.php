dcb5d216d4c13ddc9d2ae87a8007003f85955593
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-10-10 15:32:41 +0000

    images all page

--- lesson-6/blue.php
+++ lesson-6/blue.php
@@ -11,6 +11,7 @@
 	<body id="section4">
 		<!-- Page-level header -->
 		<header>
+			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
