65a0f3b0596b5c36e1f41c89750e5d05dc13254b
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-11-28 12:20:20 -0500

    lab e actually added

---
+++ lab-e/js/current-page-nav.js
@@ -0,0 +1,15 @@
+// STEP C: Get the current URL for the page
+
+// STEP D: Grab all the top-level <a> elements inside the <nav> element at the top of the page (inside the <header> element)
+
+// STEP E: Create a place to store the href value for each <a> element
+
+// STEP F: For each <a> element in the NodeList navLinks, compare the href property with the URL for the current page
+
+    // STEP G: Grab the href property of each <a> (element)
+    
+    // STEP H: Check for a match with the current page
+    
+        // STEP I: They match - so this <a> is the link for the current page
+        
+    
