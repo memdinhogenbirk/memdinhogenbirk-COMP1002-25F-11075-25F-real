d61b2c79cccefaf21f05edce65408798a4e3a96a
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-12-02 22:26:12 -0500

    passive aggresively adding some annotations

--- Project-1/js/scripter.js
+++ Project-1/js/scripter.js
@@ -12,11 +12,12 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     container.addEventListener('transitionend', function (){
         if(currentIndex == 3 && looper == true){
-            container.style.transition = 'none';
+            container.style.transition = 'none';//take away transition so the 4th image and the third image replace eachother instantly
             currentIndex = 0;
             nextSlide();
             void container.offsetWidth;
-            container.style.transition = 'left 0.7s ease-in-out';
+            //^this is apparently a common way to force the browser to apply css before continuing with the code, ridiculous way of adding what would just be a delay in c++. In truth, I hate that this works
+            container.style.transition = 'left 0.7s ease-in-out';//put back transition so everything keeps working as intended
         }
     });
     previous.addEventListener('click', function() {
@@ -27,10 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if(currentIndex == -1){
             currentIndex = 3;
             looper = false;
-            container.style.transition = 'none';
+            container.style.transition = 'none';//take away transition so the 4th image and the third image replace eachother instantly
             nextSlide();
             void container.offsetWidth;
-            container.style.transition = 'left 0.7s ease-in-out';
+            container.style.transition = 'left 0.7s ease-in-out';//put back transition so everything keeps working as intended
             currentIndex --;
         }
         nextSlide();
