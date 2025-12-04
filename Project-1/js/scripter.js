4e0a98c50ab1e05541f57a13ad278f05cb6df9a1
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-12-04 11:44:39 -0500

    css changes, now trying to get carousel accompanying ul to toggle slides

--- Project-1/js/scripter.js
+++ Project-1/js/scripter.js
@@ -3,20 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const previous = document.querySelector('.previous');
     const container = slides.querySelector('.slidecontainer');
     const next = document.querySelector('.next');
+    const togglers = document.querySelectorAll('.gotoslide li a');
     
-    let looper = true;
     let currentIndex = 0;
     function nextSlide() {
         slides.classList.remove('slide0', 'slide1', 'slide2', 'slide3');
         slides.classList.add('slide' + currentIndex);
     };
-    container.addEventListener('transitionend', function (){
-        if(currentIndex == 3 && looper == true){
+    container.addEventListener('transitionend', function (){//referenced https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Transitions
+        if(currentIndex == 3){
             container.style.transition = 'none';//take away transition so the 4th image and the third image replace eachother instantly
             currentIndex = 0;
             nextSlide();
             void container.offsetWidth;
-            //^this is apparently a common way to force the browser to apply css before continuing with the code, ridiculous way of adding what would just be a delay in c++. In truth, I hate that this works
+            //^this is apparently a common way to force the browser to apply css before continuing with the code. In truth, I hate that this works
+            //I googled "easiest way to force css to apply before continuing with javascript code"
             container.style.transition = 'left 0.7s ease-in-out';//put back transition so everything keeps working as intended
         }
     });
@@ -27,10 +28,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         if(currentIndex == -1){
             currentIndex = 3;
-            looper = false;
             container.style.transition = 'none';//take away transition so the 4th image and the third image replace eachother instantly
             nextSlide();
-            void container.offsetWidth;
+            void container.offsetWidth;//goofy solution
             container.style.transition = 'left 0.7s ease-in-out';//put back transition so everything keeps working as intended
             currentIndex --;
         }
