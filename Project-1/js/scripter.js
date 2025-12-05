35573d6d0185265b02f3ef75e3b32795f7a2a9a1
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-12-04 21:27:58 -0500

    tweaked news page, peripheral page, added some annotation to java

--- Project-1/js/scripter.js
+++ Project-1/js/scripter.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.getElementById('scrollingimages');
-    const previous = document.querySelector('.previous');
-    const container = slides.querySelector('.slidecontainer');
-    const next = document.querySelector('.next');
-    const togglers = document.querySelectorAll('.gotoslide li button');
-    const inactive = [document.getElementById('0'), document.getElementById('1'), document.getElementById('2')];
+    const slides = document.getElementById('scrollingimages');//acquire the main div that holds all the slides, id so class can be used to change slides
+    const previous = document.querySelector('.previous');//previous/back button
+    const container = slides.querySelector('.slidecontainer');//acquire the container div that holds all the slides
+    const next = document.querySelector('.next');//next/forward button
+    const togglers = document.querySelectorAll('.gotoslide li button');//acquire each header button
     
     let currentIndex = 0;
     function nextSlide() {
-        slides.classList.remove('slide0', 'slide1', 'slide2', 'slide3');
-        slides.classList.add('slide' + currentIndex);
-        togglers.forEach((element) => {
+        slides.classList.remove('slide0', 'slide1', 'slide2', 'slide3');//third slide is duplicate of first for transition purposes
+        slides.classList.add('slide' + currentIndex);//change slide by changing class, add current index number onto 'slide'
+        togglers.forEach((element) => {//remove active class from all toggler buttons before adding it back to one of them below
             element.classList.remove('active');
         });
-        inactive.forEach((element) => {
+        togglers.forEach((element) => {//was trying to use parse but realized I could just do this, it works and saved me time
         if(element.id == '0' && currentIndex == 0){
                 element.classList.add('active');
             };
@@ -37,29 +36,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     
-    previous.addEventListener('click', function() {
+    previous.addEventListener('click', function() {//back button decrements currentIndex  until it hits -1, then a hidden transition between the last and first slide occurs
         if(currentIndex >= 0) {
             currentIndex --;
         }
-        if(currentIndex == -1) {
+        if(currentIndex == -1) {//using same code as even listener above, but reversed. It's down here because it works down here. If it ain't broke...
             currentIndex = 3;
-            container.style.transition = 'none';//take away transition so the 4th image and the third image replace eachother instantly
+            container.style.transition = 'none';
             nextSlide();
             void container.offsetWidth;//goofy solution
-            container.style.transition = 'left 0.7s ease-in-out';//put back transition so everything keeps working as intended
+            container.style.transition = 'left 0.7s ease-in-out';
             currentIndex --;
         }
         nextSlide();
     });
-    next.addEventListener('click', function() {
+    next.addEventListener('click', function() {//forward button increments currentIndex until it hits 3, then the event listener above kicks in to prevent it from transitioning through all the slides
         if(currentIndex < 3) {
             currentIndex ++;
         }
         nextSlide();
     });
-    togglers.forEach((element) => {
+    togglers.forEach((element) => {//using the heading buttons to go to specific slides
         element.addEventListener('click', function() {
-            if(element.id == '0'){
+            if(element.id == '0'){//each button has an id corresponding to the slide it goes to
                 currentIndex = 0;
             };
             if(element.id == '1'){
