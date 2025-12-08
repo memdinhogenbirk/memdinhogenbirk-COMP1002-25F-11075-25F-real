6a87a360b82d930fcc255baaa22d3ae9250db1bd
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-12-08 10:37:10 -0500

    feedback form now fakes accepting data

--- Project-1/js/feedback.js
+++ Project-1/js/feedback.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const submitButton = document.getElementById('submit');
-    const firstNameInput = document.getElementById('firstName');
+    const firstNameInput = document.getElementById('fname');
     const thankYouMessage = document.getElementById('thankyoumsg');
 
     submitButton.addEventListener('click', function () {
         thankYouMessage.classList.add('cover');
+        const firstName = firstNameInput.value;
+        thankYouMessage.innerHTML = `<h2>Thank you for your feedback ${firstName}!</h2>` +
+                                    `<p>Your feedback will be passed on to the appropriate team.<br> We appreciate you taking the time to share your thoughts with us.</p>`;
     });    
 });
