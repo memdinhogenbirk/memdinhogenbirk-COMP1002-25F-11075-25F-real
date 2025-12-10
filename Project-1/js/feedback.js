64b84125da60448120f9b3ad3b4fb08b4929b501
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2025-12-10 12:12:45 -0500

    adding annotations

--- Project-1/js/feedback.js
+++ Project-1/js/feedback.js
@@ -9,4 +9,5 @@ document.addEventListener('DOMContentLoaded', function () {
         thankYouMessage.innerHTML = `<h2>Thank you for your feedback ${firstName}!</h2>` +
                                     `<p>Your feedback will be passed on to the appropriate team.<br> We appreciate you taking the time to share your thoughts with us.</p>`;
     });    
-});
+});//I learned .innerHTML when working on assignment 1, cool that javascript lets you change the content of an html element like this, very handy.
+//worth noting that this of course is not a proper submission handler, just a way of visually faking it. Ideally I'd be grabbing the content of the form and sending it somewhere relevant.
